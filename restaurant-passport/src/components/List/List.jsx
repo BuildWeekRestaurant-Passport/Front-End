b13ced89bd62af5c4f1b287a29389cf6f86e2e55
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-11-13 21:21:06 -0500

    Created dummy storage

--- restaurant-passport/src/components/List/List.jsx
+++ restaurant-passport/src/components/List/List.jsx
@@ -1,23 +1,37 @@
 import React, { useEffect } from 'react';
+import { connect } from "react-redux";
+
 import axios from 'axios'
 
-function List() {
+function List(props) {
 
-    useEffect(() => {
-        axios.get('https://build-restaurant-passport.herokuapp.com/cities')
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                throw (err)
-            })
-    }, [])
+    // useEffect(() => {
+    //     axios.get('https://build-restaurant-passport.herokuapp.com/cities')
+    //         .then(res => {
+    //             console.log(res)
+    //         })
+    //         .catch(err => {
+    //             throw (err)
+    //         })
+    // }, [])
+    console.log(props.places)
 
     return (
         <div>
-            <h1>test</h1>
+            {props.places.map(place => (
+                <>
+                    <h1>{place.name}</h1>
+                    <h2>{place.city}</h2>
+                </>
+            ))}
         </div>
     )
 }
 
-export default List
+function mapStateToProps(state) {
+    return {
+        places: state.places
+    }
+}
+
+export default connect(mapStateToProps)(List)
--- restaurant-passport/src/reducers/restaurants.js
+++ restaurant-passport/src/reducers/restaurants.js
@@ -7,11 +7,69 @@ import {
 export const initialState = {
   places: [
     {
-      name: "",
-      city: "",
-      address: "",
-      description: "",
-      city_id: ""
+      //   name: "",
+      //   city: "",
+      //   address: "",
+      //   description: "",
+      //   city_id: ""
+
+      name: "stuff",
+      city: "nashville",
+      address: "123street",
+      description: "blah blah",
+      city_id: "1"
+    },
+    {
+      //   name: "",
+      //   city: "",
+      //   address: "",
+      //   description: "",
+      //   city_id: ""
+
+      name: "stuff",
+      city: "nashville",
+      address: "123street",
+      description: "blah blah",
+      city_id: "1"
+    },
+    {
+      //   name: "",
+      //   city: "",
+      //   address: "",
+      //   description: "",
+      //   city_id: ""
+
+      name: "stuff",
+      city: "nashville",
+      address: "123street",
+      description: "blah blah",
+      city_id: "1"
+    },
+    {
+      //   name: "",
+      //   city: "",
+      //   address: "",
+      //   description: "",
+      //   city_id: ""
+
+      name: "stuff",
+      city: "nashville",
+      address: "123street",
+      description: "blah blah",
+      city_id: "1"
+    },
+    {
+      //   name: "",
+      //   city: "",
+      //   address: "",
+      //   description: "",
+      //   city_id: ""
+
+      name: "stuff",
+      city: "nashville",
+      address: "123street",
+      description: "blah blah",
+      city_id: "1"
     }
   ],
   isLoading: false,
@@ -20,23 +78,23 @@ export const initialState = {
 
 export function reducer(state = initialState, action) {
   switch (action.type) {
-    case FETCH_LIST_START:
-      return {
-        ...state,
-        isLoading: true
-      };
-    case FETCH_SMURFS_SUCCESS:
-      return {
-        ...state,
-        places: action.payload,
-        isLoading: false
-      };
-    case FETCH_SMURF_ERROR:
-      return {
-        ...state,
-        error: action.payload,
-        isLoading: false
-      };
+    // case FETCH_LIST_START:
+    //   return {
+    //     ...state,
+    //     isLoading: true
+    //   };
+    // case FETCH_SMURFS_SUCCESS:
+    //   return {
+    //     ...state,
+    //     places: action.payload,
+    //     isLoading: false
+    //   };
+    // case FETCH_SMURF_ERROR:
+    //   return {
+    //     ...state,
+    //     error: action.payload,
+    //     isLoading: false
+    //   };
     default:
       return state;
   }
