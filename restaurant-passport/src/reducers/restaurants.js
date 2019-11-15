591ef5b017cff91ecc072de05eee17b60989b96e
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-11-14 20:36:30 -0500

    Added places to the api storage, incorporated redux into the app to recieve the array of places as state data, have working auth api calls to access the data on list

--- restaurant-passport/src/reducers/restaurants.js
+++ restaurant-passport/src/reducers/restaurants.js
@@ -1,75 +1,14 @@
 import {
   FETCH_LIST_START,
-  FETCH_SMURFS_SUCCESS,
-  FETCH_SMURF_ERROR
+  FETCH_LIST_SUCCESS,
+  FETCH_LIST_ERROR
 } from "../actions/restaurants";
 
 export const initialState = {
   places: [
     {
-      //   name: "",
-      //   city: "",
-      //   address: "",
-      //   description: "",
-      //   city_id: ""
-
-      name: "stuff",
-      city: "nashville",
-      address: "123street",
-      description: "blah blah",
-      city_id: "1"
-    },
-    {
-      //   name: "",
-      //   city: "",
-      //   address: "",
-      //   description: "",
-      //   city_id: ""
-
-      name: "stuff",
-      city: "nashville",
-      address: "123street",
-      description: "blah blah",
-      city_id: "1"
-    },
-    {
-      //   name: "",
-      //   city: "",
-      //   address: "",
-      //   description: "",
-      //   city_id: ""
-
-      name: "stuff",
-      city: "nashville",
-      address: "123street",
-      description: "blah blah",
-      city_id: "1"
-    },
-    {
-      //   name: "",
-      //   city: "",
-      //   address: "",
-      //   description: "",
-      //   city_id: ""
-
-      name: "stuff",
-      city: "nashville",
-      address: "123street",
-      description: "blah blah",
-      city_id: "1"
-    },
-    {
-      //   name: "",
-      //   city: "",
-      //   address: "",
-      //   description: "",
-      //   city_id: ""
-
-      name: "stuff",
-      city: "nashville",
-      address: "123street",
-      description: "blah blah",
-      city_id: "1"
+      restID: null,
+      restName: ""
     }
   ],
   isLoading: false,
@@ -78,23 +17,23 @@ export const initialState = {
 
 export function reducer(state = initialState, action) {
   switch (action.type) {
-    // case FETCH_LIST_START:
-    //   return {
-    //     ...state,
-    //     isLoading: true
-    //   };
-    // case FETCH_SMURFS_SUCCESS:
-    //   return {
-    //     ...state,
-    //     places: action.payload,
-    //     isLoading: false
-    //   };
-    // case FETCH_SMURF_ERROR:
-    //   return {
-    //     ...state,
-    //     error: action.payload,
-    //     isLoading: false
-    //   };
+    case FETCH_LIST_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case FETCH_LIST_SUCCESS:
+      return {
+        ...state,
+        places: action.payload,
+        isLoading: false
+      };
+    case FETCH_LIST_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
     default:
       return state;
   }
