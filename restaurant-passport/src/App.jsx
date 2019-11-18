d501f8e51f791752cf801541638cc9c36e4c80db
Michael Lowe <mrlowememphis@yahoo.com>
2019-11-17 19:14:39 -0600

    RestCard changes issue with Loading

--- restaurant-passport/src/App.jsx
+++ restaurant-passport/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import api from '../../utils/api';
+//import api from '../utils/api';
 import { connect } from "react-redux";
-import { fetchPlaces } from '../../actions/restaurants'
-import {Link, Route} from 'react-router-dom'
-import RestCard from './RestCard'
-import axios from 'axios'
+import { fetchPlaces } from '../src/actions/restaurants';
+import {Link, Route} from 'react-router-dom';
+import RestCard from '../src/components/List/RestCard';
+import axios from 'axios';
 
 function List(props) {
 
@@ -47,39 +47,3 @@ export default connect(mapStateToProps, mapDispatchToProps)(List)
 
 
 
-// import React, { useEffect } from "react";
-// import { Link, Route } from "react-router-dom";
-// import Login from "./components/Login/Login";
-// import Signup from "./components/Signup/Signup";
-// import { getToken } from './utils/api'
-// import List from './components/List/List'
-
-
-
-// import "./App.css";
-
-// function App(props) {
-//   const signedIn = getToken()
-
-
-
-//   return (
-//     <div className="App">
-//       <nav>
-//         {!signedIn && <Link to='/'>Sign In</Link>}
-//         {signedIn && <Link to='/places'>View Restaurants</Link>}
-
-//         <Link to='/signup'>Sign Up</Link>
-//         <Link to='/'>Login</Link>
-//       </nav>
-
-//       <Route exact path="/" component={Login} />
-//       <Route exact path="/signup" component={Signup} />
-//       <Route exact path='/places' component={List} />
-//     </div>
-//   );
-// }
-
-
-
-// export default App;
--- restaurant-passport/src/components/List/RestCard.js
+++ restaurant-passport/src/components/List/RestCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 
  export default function RestCard(props) {
@@ -10,7 +10,7 @@ import styled from "styled-components"
      flex-direction: column;
      justify-content: center;
      align-items: center;
-     background-color: #11a7f7;
+     background-color:: #406098;
      margin: 5% 20%
      border: solid 4px black;
      border-radius: 8px;
@@ -21,21 +21,18 @@ import styled from "styled-components"
       -webkit-box-shadow: 7px 7px 5px #2F4F4F;
       -khtml-box-shadow: 7px 7px 7px #2F4F4F;`
 
-
      const Header = styled.h2`
      font-size: 35px;
      text-shadow: 2px 2px BlueViolet;`
-
 
    return(
      <Card>
 
        <Header>{props.name}</Header>
-         <p>Name: {props.species}</p>
-         <p>Description: {props.status}</p>
+         <p>Name: {props.name}</p>
+         <p>Description: {props.description}</p>
          <p>Location: {props.location}</p>
-         <p>Address: {props.location}</p>
-
+         <p>Address: {props.address}</p>
 
      </Card>
 
