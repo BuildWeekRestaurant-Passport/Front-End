6da0f493f1f9d8319110f7fa6377ff931ae7e8f4
Benjamin Earley <benjaminearley@Benjamins-MacBook-Pro-2.local>
2019-11-20 19:45:33 -0500

    Fixed error with styled-components.

--- restaurant-passport/src/components/Description/Description.jsx
+++ restaurant-passport/src/components/Description/Description.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import api from '../../utils/api'
-// import styled from "styled-components"
+import styled from "styled-components"
 
 function Description(props) {
     const [place, setPlace] = useState();
@@ -39,26 +39,26 @@ function Description(props) {
     };
 
 
-    // const Card = styled.div`
-    //  display: flex;
-    //  flex-wrap: wrap;
-    //  flex-direction: column;
-    //  justify-content: center;
-    //  align-items: center;
-    //  background-color: #f0e5e5;
-    //  margin: 5% 20%;
-    //  border: solid 4px black;
-    //  border-radius: 8px;
-    //  font-weight: 600;
-    //  font-size: 18px;
-    //  box-shadow: 7px 7px 5px #2F4F4F;
-    //   -moz-box-shadow: 7px 7px 5px #2F4F4F;
-    //   -webkit-box-shadow: 7px 7px 5px #2F4F4F;
-    //   -khtml-box-shadow: 7px 7px 7px #2F4F4F;`
+    const Card = styled.div`
+     display: flex;
+     flex-wrap: wrap;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     background-color: #f0e5e5;
+     margin: 5% 20%;
+     border: solid 4px black;
+     border-radius: 8px;
+     font-weight: 600;
+     font-size: 18px;
+     box-shadow: 7px 7px 5px #2F4F4F;
+      -moz-box-shadow: 7px 7px 5px #2F4F4F;
+      -webkit-box-shadow: 7px 7px 5px #2F4F4F;
+      -khtml-box-shadow: 7px 7px 7px #2F4F4F;`
 
-    // const Header = styled.h2`
-    //  font-size: 35px;
-    //  text-shadow: 2px 2px BlueViolet;`
+    const Header = styled.h2`
+     font-size: 35px;
+     text-shadow: 2px 2px BlueViolet;`
 
 
     if (!place) {
